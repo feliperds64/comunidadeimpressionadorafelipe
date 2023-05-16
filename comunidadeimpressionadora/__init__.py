563c7be49991db41fdf0b006a1657a89f67fecb6
Felipe <133708294+feliperds64@users.noreply.github.com>
2023-05-16 16:03:31 -0300

    Update __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 
 
 app.config['SECRET_KEY'] = '19b253df6e070936e5e83ba04d6e04c7'
-#if os.getenv("DATABASE_URL"):
-#    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-#else:
-#    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://comunidadeimpressionadora:OqCwB30dzmso5l2hIyfasuuTDuOnReAQ@dpg-chhah9u4dad31tjuoncg-a/comunidadeimpressionadora'    
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://comunidadeimpressionadora:OqCwB30dzmso5l2hIyfasuuTDuOnReAQ@dpg-chhah9u4dad31tjuoncg-a/comunidadeimpressionadora'    
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
